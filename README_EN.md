cbfbe6674e0fd2f66526fd5798b6f238ccf46591
ilyashirko <ilyashirko@gmail.com>
2022-02-05 18:05:49 +0300

    bit.ly API

---
+++ README_EN.md
@@ -0,0 +1,37 @@
+# Clipping links with bit.ly
+Generates short links bit.ly and counts the number of clicks on such links
+
+## How to install
+###You will need a token to work.
+You can get it here:
+>https://app.bitly.com/settings/api/
+Register, enter a password and click "Generate token"
+The token looks something like this: >"3ig534hv53uv5ih4gjh34gb5jh3bjhb345b3j5b"
+It must be placed in the ".env" file after the "=" sign
+It should turn out like this:
+>BITLINK_TOKEN=3ig534hv53uv5ih4gjh34gb5jh3bjhb345b3j5b
+
+###Installing dependencies.
+You will need python3 and pip (pip3).
+> pip3 install -r requirement.txt
+
+## How to start
+instead of LINK, enter a link in the format "http://www.google.com"
+>$python3 main.py LINK
+
+
+# Description of main.py functions
+## is_bitlink
+Checks if the link is shortened.
+If it is a bitlink, returns True, otherwise False
+
+## count_click
+If the link you entered is abbreviated:
+ - requests the number of transitions on it;
+ - displays on the screen in case of a successful server response.
+ - displays an error if there is no response.
+
+## shorten_link
+Requests a shortened link from the server for the entered link.
+The link must be specified in full, with the protocol, otherwise an error will be displayed.
+Displays the received link
---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import argparse
+
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+import requests
+
+load_dotenv()
+TOKEN = os.getenv('BITLINK_TOKEN')
+BITLY_HEADERS = {'Authorization': f'Bearer {TOKEN}'}
+
+
+def shorten_link(entered_link):
+    response = requests.post(
+        url='https://api-ssl.bitly.com/v4/bitlinks',
+        headers=BITLY_HEADERS,
+        json={'long_url': entered_link}
+    )
+    response.raise_for_status()
+    return response.json()["id"]
+    
+
+def count_click(parced_bitlink):
+    bitlink = f'{parced_bitlink.netloc}{parced_bitlink.path}'
+    response = requests.get(
+        url=f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary',
+        headers=BITLY_HEADERS
+    )
+    response.raise_for_status()
+    return response.json()["total_clicks"]
+
+
+def is_bitlink(parsed_link):
+    link = f'{parsed_link.netloc}{parsed_link.path}'
+    response = requests.get(
+        url=f'https://api-ssl.bitly.com/v4/bitlinks/{link}',
+        headers=BITLY_HEADERS
+    )
+    return response.ok
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Программа сокращает ссылки и показывает количество переходов по сокращенным ссылкам')
+    parser.add_argument('link', help='https://www.google.com')
+    args = parser.parse_args()
+    link = args.link
+    #link = input('Введите url для сокращения (напр. https://www.google.com): ')
+    if is_bitlink(urlparse(link)):
+        try:
+            clicks = count_click(urlparse(link))
+            print(f'Количество переходов: {clicks}')
+        except requests.exceptions.HTTPError as error:
+            exit('Сервер не отвечает:\n{0}'.format(error))
+    else:
+        try:
+            bitlink = shorten_link(link)
+            print(f'Битлинк: {bitlink}')
+        except requests.exceptions.HTTPError as error:
+            exit('ОШИБКА: Неверный формат ссылки, либо не отвечает сервер:\n{0}'.format(error))
